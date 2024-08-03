f94ad868e598af48c96fc12bb41752dd7cdc765a
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-08-03 04:40:30 -0500

    hii

--- tyem-pos/src/app/pages/home/Home.jsx
+++ tyem-pos/src/app/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import HomeCartSection from "./sections/cart/HomeCartSection";
 import HomeCategorySection from "./sections/HomeCategorySection";
