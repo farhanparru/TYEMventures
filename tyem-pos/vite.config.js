cd4b9776b3b8e6272c204ec3a6ea3a901de5719b
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-08-06 07:09:10 -0500

    socket

--- tyem-pos/vite.config.js
+++ tyem-pos/vite.config.js
@@ -4,9 +4,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Expose to the network
     port: 5173,      // Default port
-    hmr: {
-      host: 'ventrues.invenro.site', // Use your domain name here
-      port: 443,                     // Use 443 for HTTPS or 80 for HTTP
-    },
+   
   },
 });
