b4e815bdf28f5f80648a776680fa4f36867ecc1c
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-08-06 04:53:46 -0500

    webSocket

--- tyem-pos/vite.config.js
+++ tyem-pos/vite.config.js
@@ -4,5 +4,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Expose to the network
     port: 5173,      // Default port
-  },
+    hmr: {
+      host: 'ventrues.invenro.site', // Set the correct host for HMR
+      port: 443,                     // Use port 443 if your site is served over HTTPS
+      protocol: 'wss',               // Use WebSocket Secure (wss) if your site uses HTTPS
+    },
+  },
 });
