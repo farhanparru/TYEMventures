40896a085ecb7e0ad7f99e95881592e93ae3c69e
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-08-06 04:32:59 -0500

    all

--- tyem-pos/src/app/pages/auth/FormAction.jsx
+++ tyem-pos/src/app/pages/auth/FormAction.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 export default function FormAction({
@@ -13,7 +12,7 @@ export default function FormAction({
             type==='Button' ?
             <button
                 type={action}
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-chicket-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 mt-10"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-chicket-600 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 mt-10"
                 onSubmit={handleSubmit}
             >
 
