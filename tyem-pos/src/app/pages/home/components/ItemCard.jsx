17e38e5a09f4ad1a96937a3aac7bc7f4946bc56a
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-08-13 06:46:34 -0500

    scroller update

--- tyem-pos/src/app/pages/home/components/ItemCard.jsx
+++ tyem-pos/src/app/pages/home/components/ItemCard.jsx
@@ -37,16 +37,15 @@ const ItemCard = () => {
   return (
     <div
       onClick={onItemClick}
-      className="home__item flex flex-col gap-2 justify-around bg-ch-headers-500 text-white p-3 rounded-md shadow-xl cursor-pointer transition-all ease-in-out hover:bg-slate-300 hover:scale-95 hover:shadow-sm"
+        className="home__item flex flex-col gap-2 justify-around bg-ch-headers-500 text-white p-3 rounded-md shadow-xl cursor-pointer transition-all ease-in-out hover:bg-slate-300 hover:scale-95 hover:shadow-sm"
       style={{ height: '300px', overflowY: 'auto' }} // Set the height and enable vertical scroll
     >
       {items.map((item) => (
-        <div key={item._id} className="flex flex-col items-center">
-        
+        <div key={item._id} >
           <h3 className="text-sm font-bold capitalize">{item.ItemName}</h3>
           <p className="text-md font-semibold">₹ {item.Price.toFixed(2)}</p>
         </div>
-      ))
+      ))}
     </div>
   );
 };
