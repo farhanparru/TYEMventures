c39108d8da0eb9e198600732cba77a3a8bd94451
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-08-13 06:08:56 -0500

    update isArray

--- tyem-pos/src/app/pages/home/components/ItemCard.jsx
+++ tyem-pos/src/app/pages/home/components/ItemCard.jsx
@@ -5,18 +5,27 @@ import { addToCart } from "../store/cartSlice";
 
 const ItemCard = () => {
   const [item, setItem] = useState(null);
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
     axios.get('https://tyem.invenro.site/api/user/ExcelItems') // Adjust your API endpoint
       .then((response) => {
-        console.log(response,"hai");
-        setItem(response.data.item); // Adjust according to your API response
+        if (response.data && Array.isArray(response.data)) {
+          setItems(response.data);
+          console.log(response.data);
+          
+        } else {
+          setItems([]);
+        }
+        setLoading(false);
       })
-
-      
       .catch((error) => {
         console.error('There was an error fetching the items!', error);
+        setError('Error fetching items');
+        setLoading(false);
       });
   }, []);
 
@@ -26,19 +35,31 @@ const ItemCard = () => {
     }
   };
 
-  // if (!item) return <div>Loading...</div>; // Or some loading spinner
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
 
+  // if (!item) return <div>Loading...</div>; // Or some loading spinner
   return (
-    <div
-      onClick={onItemClick}
-      className="home__item flex flex-col gap-2 justify-around bg-ch-headers-500 text-white p-3 rounded-md shadow-xl cursor-pointer transition-all ease-in-out hover:bg-slate-300 hover:scale-95 hover:shadow-sm"
-    >
-      <h3 className="text-sm font-bold capitalize">
-        {item?.ItemName}
-      </h3>
-      <p className="text-md">₹ {item?.Price.toFixed(2)}</p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      {items.length === 0 ? (
+        <div>No items available</div>
+      ) : (
+        items.map((item) => (
+          <div
+            key={item._id} // Use _id as the key
+            onClick={() => onItemClick(item)}
+            className="home__item flex flex-col gap-2 justify-around bg-ch-headers-500 text-white p-3 rounded-md shadow-xl cursor-pointer transition-all ease-in-out hover:bg-slate-300 hover:scale-95 hover:shadow-sm"
+          >
+            <h3 className="text-sm font-bold capitalize">
+              {item.ItemName}
+            </h3>
+            <p className="text-md">₹ {item.Price.toFixed(2)}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
+
 
 export default ItemCard;
