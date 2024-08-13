108db3a59f1e75aad2669f060412e5a4e23b2a8b
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-08-13 06:16:34 -0500

    new

--- tyem-pos/src/app/pages/home/components/ItemCard.jsx
+++ tyem-pos/src/app/pages/home/components/ItemCard.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import { addToCart } from "../store/cartSlice";
 
 const ItemCard = () => {
-  const [item, setItem] = useState(null);
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -16,7 +15,6 @@ const ItemCard = () => {
         if (response.data && Array.isArray(response.data)) {
           setItems(response.data);
           console.log(response.data);
-          
         } else {
           setItems([]);
         }
@@ -29,37 +27,29 @@ const ItemCard = () => {
       });
   }, []);
 
-  const onItemClick = () => {
-    if (item) {
-      dispatch(addToCart(item));
-    }
+  const onItemClick = (item) => {
+    dispatch(addToCart(item));
   };
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
-  // if (!item) return <div>Loading...</div>; // Or some loading spinner
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-      {items.length === 0 ? (
-        <div>No items available</div>
-      ) : (
-        items.map((item) => (
-          <div
-            key={item._id} // Use _id as the key
-            onClick={() => onItemClick(item)}
-            className="home__item flex flex-col gap-2 justify-around bg-ch-headers-500 text-white p-3 rounded-md shadow-xl cursor-pointer transition-all ease-in-out hover:bg-slate-300 hover:scale-95 hover:shadow-sm"
-          >
-            <h3 className="text-sm font-bold capitalize">
-              {item.ItemName}
-            </h3>
-            <p className="text-md">₹ {item.Price.toFixed(2)}</p>
-          </div>
-        ))
-      )}
+    <div className="item-card-container overflow-y-auto max-h-[500px] p-4">
+      {items.map((item) => (
+        <div
+          key={item._id} // Use _id as the key
+          onClick={() => onItemClick(item)}
+          className="home__item flex flex-col gap-2 justify-around bg-ch-headers-500 text-white p-3 rounded-md shadow-xl cursor-pointer transition-transform duration-300 hover:bg-slate-300 hover:scale-95 hover:shadow-sm mb-2"
+        >
+          <h3 className="text-sm font-bold capitalize">
+            {item.ItemName}
+          </h3>
+          <p className="text-md">₹ {item.Price.toFixed(2)}</p>
+        </div>
+      ))}
     </div>
   );
 };
-
 
 export default ItemCard;
