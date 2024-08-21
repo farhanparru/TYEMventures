1ffd4ec4a0b5b85a11d8b8c5ee6a5f501af71cfb
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-08-21 07:03:08 -0500

    renderDetails

--- tyem-pos/src/app/pages/home/sections/body/components/Salessecstion.jsx
+++ tyem-pos/src/app/pages/home/sections/body/components/Salessecstion.jsx
@@ -1,14 +1,24 @@
-import React, { useState ,useEffect} from "react";
-import { FaCheckCircle, FaCalendar, FaClock, FaChevronDown } from 'react-icons/fa';
+import React, { useState, useEffect } from "react";
+import {
+  FaCheckCircle,
+  FaCalendar,
+  FaClock,
+  FaChevronDown,
+} from "react-icons/fa";
 import { Dropdown, Menu } from "antd";
-import { FaPrint, FaDownload,FaUndoAlt } from 'react-icons/fa'; // Ensure correct imports
-import { MailOutlined, MessageOutlined, WhatsAppOutlined , UpOutlined, DownOutlined } from '@ant-design/icons'; // Import from @ant-design/icons
+import { FaPrint, FaDownload, FaUndoAlt } from "react-icons/fa"; // Ensure correct imports
+import {
+  MailOutlined,
+  MessageOutlined,
+  WhatsAppOutlined,
+  UpOutlined,
+  DownOutlined,
+} from "@ant-design/icons"; // Import from @ant-design/icons
 
 import axios from "axios";
 import { DateTime } from "luxon";
-import jsPDF from 'jspdf';
-import 'jspdf-autotable';
-
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 const OrderItem = ({ order, onClick, selected }) => {
   let formattedDate = "";
@@ -16,12 +26,16 @@ const OrderItem = ({ order, onClick, selected }) => {
 
   // Check if the order is PosOrder or WhatsAppOrder and format the date/time accordingly
   if (order.orderType === "PosOrder") {
-    const utcDate = DateTime.fromISO(order.orderDetails?.orderDate, { zone: "utc" });
+    const utcDate = DateTime.fromISO(order.orderDetails?.orderDate, {
+      zone: "utc",
+    });
     const zonedDate = utcDate.setZone("Asia/Kolkata");
     formattedDate = zonedDate.toFormat("MMM dd, yyyy");
     formattedTime = zonedDate.toFormat("hh:mm:ss a");
   } else if (order.orderType === "WhatsAppOrder") {
-    const utcDate = DateTime.fromISO(order.orderMeta?.orderDate, { zone: "utc" });
+    const utcDate = DateTime.fromISO(order.orderMeta?.orderDate, {
+      zone: "utc",
+    });
     const zonedDate = utcDate.setZone("Asia/Kolkata");
     formattedDate = zonedDate.toFormat("MMM dd, yyyy");
     formattedTime = zonedDate.toFormat("hh:mm:ss a");
@@ -38,32 +52,45 @@ const OrderItem = ({ order, onClick, selected }) => {
         ${selected ? "" : "hover:bg-blue-100 hover:border-blue-300"}
       `}
       onClick={() => onClick(order)}
-      aria-label={`Order ${order.orderMeta?.posOrderId || order.orderDetails?.orderNumber} details`}
+      aria-label={`Order ${
+        order.orderMeta?.posOrderId || order.orderDetails?.orderNumber
+      } details`}
     >
       {/* Smaller Badge for Order Type */}
       <span
-        className={`bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ${order.orderType === 'WhatsAppOrder' ? 'bg-red-500' : 'bg-green-500'}`}
+        className={`absolute top-2 left-2 transform px-2 py-0.5 text-[10px] font-semibold text-white rounded-full ${
+          order.orderType === "WhatsAppOrder" ? "bg-red-500" : "bg-green-500"
+        }`}
       >
-        {order.orderType === 'WhatsAppOrder' ? 'WhatsApp Order' : 'POS Order'}
+        {order.orderType === "WhatsAppOrder" ? "WhatsApp Order" : "POS Order"}
       </span>
-  
+
       <div className="flex-1">
         <h3 className="text-lg font-semibold">
-          Order #{order.orderMeta?.posOrderId || order.orderDetails?.orderNumber}
+          Order #
+          {order.orderMeta?.posOrderId || order.orderDetails?.orderNumber}
         </h3>
         <p className="text-sm">
-          {order.totalQuantity} Item{order.totalQuantity > 1 ? 's' : ''} | 
-          {order.orderMeta?.paymentTendered || order.total} {order.orderDetails[0]?.product_currency || 'INR'} | 
+          {order.totalQuantity} Item{order.totalQuantity > 1 ? "s" : ""} |
+          {order.orderMeta?.paymentTendered || order.total}{" "}
+          {order.orderDetails[0]?.product_currency || "INR"} |
           {order.orderMeta?.orderType || order.method}
         </p>
-  
+
         <div className="flex items-center mt-2">
-          <span className={`px-2 py-1 text-xs font-semibold rounded ${order.orderMeta?.paymentStatus === 'Completed' || order.orderDetails.paymentStatus === 'COMPLETED' ? 'bg-green-100 text-green-800' : 'bg-red-200 text-red-800'}`}>
+          <span
+            className={`px-2 py-1 text-xs font-semibold rounded ${
+              order.orderMeta?.paymentStatus === "Completed" ||
+              order.orderDetails.paymentStatus === "COMPLETED"
+                ? "bg-green-100 text-green-800"
+                : "bg-red-200 text-red-800"
+            }`}
+          >
             {order.orderMeta?.paymentStatus || order.status}
           </span>
         </div>
       </div>
-      
+
       <div className="text-right">
         <h1 className="text-md text-black">
           <FaCalendar className="inline mr-1" />
@@ -76,25 +103,27 @@ const OrderItem = ({ order, onClick, selected }) => {
       </div>
     </div>
   );
-
 };
-
 
 const OrderDetails = ({ order }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   // Declare formattedDate and formattedTime variables
-  let formattedDate = '';
-  let formattedTime = '';
+  let formattedDate = "";
+  let formattedTime = "";
 
   // Check if the order is PosOrder or WhatsAppOrder and format the date/time accordingly
   if (order?.orderType === "PosOrder") {
-    const utcDate = DateTime.fromISO(order.orderDetails?.orderDate, { zone: "utc" });
+    const utcDate = DateTime.fromISO(order.orderDetails?.orderDate, {
+      zone: "utc",
+    });
     const zonedDate = utcDate.setZone("Asia/Kolkata");
     formattedDate = zonedDate.toFormat("MMM dd, yyyy");
     formattedTime = zonedDate.toFormat("hh:mm:ss a");
   } else if (order?.orderType === "WhatsAppOrder") {
-    const utcDate = DateTime.fromISO(order.orderMeta?.orderDate, { zone: "utc" });
+    const utcDate = DateTime.fromISO(order.orderMeta?.orderDate, {
+      zone: "utc",
+    });
     const zonedDate = utcDate.setZone("Asia/Kolkata");
     formattedDate = zonedDate.toFormat("MMM dd, yyyy");
     formattedTime = zonedDate.toFormat("hh:mm:ss a");
@@ -119,15 +148,26 @@ const OrderDetails = ({ order }) => {
     // Add order details
     doc.autoTable({
       startY: 50,
-      head: [['Invoice Number', 'Notes', 'Subtotal', 'Delivery Charge', 'Total Amount', 'Payment Method']],
+      head: [
+        [
+          "Invoice Number",
+          "Notes",
+          "Subtotal",
+          "Delivery Charge",
+          "Total Amount",
+          "Payment Method",
+        ],
+      ],
       body: [
         [
-          order?.orderMeta?.posOrderId || 'N/A',
+          order?.orderMeta?.posOrderId || "N/A",
           "WhatsAppOrder",
-          `${order?.orderDetails?.[0]?.unit_price || '0'} INR`,
+          `${order?.orderDetails?.[0]?.unit_price || "0"} INR`,
           "98 INR",
-          `${order?.orderMeta?.paymentTendered || '0'} ${order?.orderDetails?.[0]?.product_currency || 'INR'}`,
-          order?.orderMeta?.paymentMethod || 'Cash',
+          `${order?.orderMeta?.paymentTendered || "0"} ${
+            order?.orderDetails?.[0]?.product_currency || "INR"
+          }`,
+          order?.orderMeta?.paymentMethod || "Cash",
         ],
       ],
     });
@@ -136,9 +176,14 @@ const OrderDetails = ({ order }) => {
     doc.text("Customer Details", 20, doc.lastAutoTable.finalY + 20);
     doc.autoTable({
       startY: doc.lastAutoTable.finalY + 30,
-      head: [['Name', 'Phone Number', 'Place', 'Order Outstanding']],
+      head: [["Name", "Phone Number", "Place", "Order Outstanding"]],
       body: [
-        [order?.customer?.name || 'N/A', order?.customer?.phone || 'N/A', "Kasaragod", ""],
+        [
+          order?.customer?.name || "N/A",
+          order?.customer?.phone || "N/A",
+          "Kasaragod",
+          "",
+        ],
       ],
     });
 
@@ -147,13 +192,18 @@ const OrderDetails = ({ order }) => {
   };
 
   // Define delivery charge and discount values
-  const deliveryCharge = '98 INR';
-  const discountValue = order?.discount?.type === 'fixed'  ? `${order.discount.value} INR` : deliveryCharge;
+  const deliveryCharge = "98 INR";
+  const discountValue =
+    order?.discount?.type === "fixed"
+      ? `${order.discount.value} INR`
+      : deliveryCharge;
 
   return (
     <div className="p-6 bg-white rounded-lg shadow-lg border border-gray-200 max-w-3xl mx-auto">
       <div className="mb-8">
-        <h3 className="text-2xl font-bold mb-4 border-b pb-2">Payment Details</h3>
+        <h3 className="text-2xl font-bold mb-4 border-b pb-2">
+          Payment Details
+        </h3>
         <div className="relative mb-8">
           <button
             onClick={toggleDropdown}
@@ -182,11 +232,12 @@ const OrderDetails = ({ order }) => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
           <div>
             <h4 className="font-semibold">Invoice Number</h4>
-            Order #{order?.orderMeta?.orderType || order?.orderDetails?.invoiceNumber}
+            Order #
+            {order?.orderMeta?.orderType || order?.orderDetails?.invoiceNumber}
           </div>
           <div>
             <h4 className="font-semibold">OrderType</h4>
-            Order #{order?.type || 'WhatsAppOrder'}
+            Order #{order?.type || "WhatsAppOrder"}
           </div>
           <div className="text-right md:text-left">
             <h1 className="text-md text-black">
@@ -200,7 +251,11 @@ const OrderDetails = ({ order }) => {
           </div>
           <div>
             <h4 className="font-semibold">Subtotal</h4>
-            <p>{order?.orderDetails?.[0]?.unit_price || order?.itemDetails?.total} INR</p>
+            <p>
+              {order?.orderDetails?.[0]?.unit_price ||
+                order?.itemDetails?.total}{" "}
+              INR
+            </p>
           </div>
           <div>
             <h4 className="font-semibold">Delivery Charge</h4>
@@ -208,17 +263,21 @@ const OrderDetails = ({ order }) => {
           </div>
           <div>
             <h4 className="font-semibold">Total Amount</h4>
-            <p>{order?.orderMeta?.paymentTendered || order?.itemDetails?.total}</p>
+            <p>
+              {order?.orderMeta?.paymentTendered || order?.itemDetails?.total}
+            </p>
           </div>
           <div>
             <h4 className="font-semibold">Payment Method</h4>
-            <p>{order?.orderMeta?.paymentMethod || 'Cash'}</p>
+            <p>{order?.orderMeta?.paymentMethod || "Cash"}</p>
           </div>
         </div>
       </div>
 
       <div className="mb-8">
-        <h3 className="text-2xl font-bold mb-4 border-b pb-2">Customer Details</h3>
+        <h3 className="text-2xl font-bold mb-4 border-b pb-2">
+          Customer Details
+        </h3>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
           <div>
             <h4 className="font-semibold">Name</h4>
@@ -226,7 +285,7 @@ const OrderDetails = ({ order }) => {
           </div>
           <div>
             <h4 className="font-semibold">Phone Number</h4>
-            <p>{order?.customer?.phone || 'N/A'}</p>
+            <p>{order?.customer?.phone || "N/A"}</p>
           </div>
           <div>
             <h4 className="font-semibold">Place</h4>
@@ -252,9 +311,6 @@ const OrderDetails = ({ order }) => {
     </div>
   );
 };
-
-
-
 
 const CartSection = ({ order }) => {
   const [dropdownVisible, setDropdownVisible] = useState(false);
@@ -287,11 +343,10 @@ const CartSection = ({ order }) => {
   //   );
   // }
 
-   // Handle item details based on the order type
-   const renderItemDetails = () => {
+  const renderItemDetails = () => {
     if (!order) return null;
-  
-    if (order.orderType === 'PosOrder') {
+
+    if (order.orderType === "PosOrder") {
       console.log("PosOrder items:", order.itemDetails.items);
 
       if (Array.isArray(order.itemDetails.items)) {
@@ -308,12 +363,11 @@ const CartSection = ({ order }) => {
       } else {
         return <p>No items found.</p>;
       }
-    } else if (order.orderType === 'WhatsAppOrder') {
+    } else if (order.orderType === "WhatsAppOrder") {
+      console.log("WhatsAppOrder items:", order.orderDetails);
 
-      console.log("WhatsAppOrder items:", order.orderDetails?.items);
-
-      if (Array.isArray(order.orderDetails?.items)) {
-        return order.orderDetails.items.map((item, index) => (
+      if (Array.isArray(order.orderDetails)) {
+        return order.orderDetails.map((item, index) => (
           <div
             key={index}
             className="flex items-center justify-between p-4 bg-white rounded-md text-black mb-4"
@@ -330,18 +384,22 @@ const CartSection = ({ order }) => {
     }
     return null;
   };
-    // Handle the total calculation based on order type
-    const calculateTotal = () => {
-      if (!order) return 0;
-  
-      if (order.orderType === 'PosOrder') {
-        return order.itemDetails.total; // Use the total from PosOrder details
-      } else if (order.orderType === 'WhatsAppOrder') {
-        // Assuming you have a way to calculate total for WhatsAppOrder
-        return order.orderDetails.reduce((acc, item) => acc + (item.unit_price * item.product_quantity), 0);
-      }
-      return 0;
-    };
+
+  // Handle the total calculation based on order type
+  const calculateTotal = () => {
+    if (!order) return 0;
+
+    if (order.orderType === "PosOrder") {
+      return order.itemDetails.total; // Use the total from PosOrder details
+    } else if (order.orderType === "WhatsAppOrder") {
+      // Assuming you have a way to calculate total for WhatsAppOrder
+      return order.orderDetails.reduce(
+        (acc, item) => acc + item.unit_price * item.product_quantity,
+        0
+      );
+    }
+    return 0;
+  };
 
   return (
     <div className="flex flex-col h-full p-4 bg-gray-800 text-white">
@@ -350,18 +408,25 @@ const CartSection = ({ order }) => {
       </div>
 
       {/* Summary and Actions */}
-      <div className="mt-auto p-4 bg-gray-700 text-white rounded-lg" style={{ marginBottom: "42px" }}>
+      <div
+        className="mt-auto p-4 bg-gray-700 text-white rounded-lg"
+        style={{ marginBottom: "42px" }}
+      >
         <div className="flex justify-between mb-4">
           <span className="font-semibold">Subtotal</span>
           <span>
-            {order?.orderMeta?.paymentTendered || order?.itemDetails?.total} {order?.orderDetails[0]?.product_currency || 'INR'}
+            {order?.orderMeta?.paymentTendered || order?.itemDetails?.total}{" "}
+            {order?.orderDetails[0]?.product_currency || "INR"}
           </span>
         </div>
 
         <div className="flex justify-between items-center mb-4">
           <span className="font-semibold">Total</span>
           <span>
-            <h1>{calculateTotal()} {order?.orderDetails[0]?.product_currency || 'INR'}</h1>
+            <h1>
+              {calculateTotal()}{" "}
+              {order?.orderDetails[0]?.product_currency || "INR"}
+            </h1>
           </span>
         </div>
 
@@ -371,12 +436,7 @@ const CartSection = ({ order }) => {
             <FaUndoAlt className="mr-2" /> Refund
           </button>
           <Dropdown
-            overlay={
-              <div className="p-2">
-                <button className="w-full px-4 py-2 text-gray-700 hover:bg-gray-100">Print</button>
-                <button className="w-full px-4 py-2 text-gray-700 hover:bg-gray-100">Download</button>
-              </div>
-            }
+            overlay={menu} // Replace the inline dropdown content with the menu variable
             trigger={["click"]}
             onVisibleChange={(visible) => setDropdownVisible(visible)}
           >
@@ -394,20 +454,21 @@ const CartSection = ({ order }) => {
     </div>
   );
 };
+
 const SalesSection = () => {
   const [orders, setOrders] = useState([]);
   const [selectedOrder, setSelectedOrder] = useState(null);
 
   // WebSocket for WhatsAppOrder real-time updates
   useEffect(() => {
-    const wsWhatsApp = new WebSocket('wss://tyem.invenro.site');
+    const wsWhatsApp = new WebSocket("wss://tyem.invenro.site");
 
     wsWhatsApp.onmessage = (event) => {
       const newOrder = JSON.parse(event.data);
-      console.log('New WhatsApp WebSocket Order:', newOrder);
+      console.log("New WhatsApp WebSocket Order:", newOrder);
 
-      if (newOrder.orderMeta.paymentStatus === 'Completed') {
-        newOrder.orderType = 'WhatsAppOrder';
+      if (newOrder.orderMeta.paymentStatus === "Completed") {
+        newOrder.orderType = "WhatsAppOrder";
         setOrders((prevOrders) => [newOrder, ...prevOrders]);
       }
     };
@@ -419,14 +480,14 @@ const SalesSection = () => {
 
   // WebSocket for PosOrder real-time updates
   useEffect(() => {
-    const wsPosOrder = new WebSocket('wss://tyem.invenro.site/pos');
+    const wsPosOrder = new WebSocket("wss://tyem.invenro.site/pos");
 
     wsPosOrder.onmessage = (event) => {
       const newOrder = JSON.parse(event.data);
-      console.log('New PosOrder WebSocket Order:', newOrder);
+      console.log("New PosOrder WebSocket Order:", newOrder);
 
-      if (newOrder.status === 'COMPLETED') {
-        newOrder.orderType = 'PosOrder';
+      if (newOrder.status === "COMPLETED") {
+        newOrder.orderType = "PosOrder";
         setOrders((prevOrders) => [newOrder, ...prevOrders]);
       }
     };
@@ -441,30 +502,37 @@ const SalesSection = () => {
     const fetchOrders = async () => {
       try {
         // Fetch WhatsApp Orders
-        const responseWhatsApp = await axios.get('https://tyem.invenro.site/api/tyem/Whatsappget');
-        console.log('WhatsApp API Response:', responseWhatsApp.data);
+        const responseWhatsApp = await axios.get(
+          "https://tyem.invenro.site/api/tyem/Whatsappget"
+        );
+        console.log("WhatsApp API Response:", responseWhatsApp.data);
 
         // Fetch POS Orders
-        const responsePosOrder = await axios.get('https://tyem.invenro.site/api/user/PosOrder');
-        console.log('POS API Response:', responsePosOrder.data);
+        const responsePosOrder = await axios.get(
+          "https://tyem.invenro.site/api/user/PosOrder"
+        );
+        console.log("POS API Response:", responsePosOrder.data);
 
         // Filter completed orders and add orderType
-        const completedWhatsAppOrders = responseWhatsApp.data.filter(order => order.orderMeta.paymentStatus === 'Completed').map(order => ({
-          ...order,
-          orderType: 'WhatsAppOrder',
-        }));
+        const completedWhatsAppOrders = responseWhatsApp.data
+          .filter((order) => order.orderMeta.paymentStatus === "Completed")
+          .map((order) => ({
+            ...order,
+            orderType: "WhatsAppOrder",
+          }));
 
-        const completedPosOrders = responsePosOrder.data.filter(order => order.orderDetails.paymentStatus === 'COMPLETED').map(order => ({
-          ...order,
-          orderType: 'PosOrder',
-        }));
+        const completedPosOrders = responsePosOrder.data
+          .filter((order) => order.orderDetails.paymentStatus === "COMPLETED")
+          .map((order) => ({
+            ...order,
+            orderType: "PosOrder",
+          }));
 
-        console.log(completedPosOrders,"completedPosOrders");
-        
+        console.log(completedPosOrders, "completedPosOrders");
 
         setOrders([...completedWhatsAppOrders, ...completedPosOrders]);
       } catch (error) {
-        console.error('Error fetching orders:', error);
+        console.error("Error fetching orders:", error);
       }
     };
 
